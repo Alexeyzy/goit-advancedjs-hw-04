e5b163c9dc20e0cef4133ac858898b34e11549f5
Alexey <a03221983@gmail.com>
2025-11-09 20:50:48 +0200

    44

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com';
+axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '53124504-9ef9dc7d083288085c55b9d9d';
 const PER_PAGE = 15;
 
 export async function fetchPhotosByQuery(query, page = 1) {
   try {
-    const response = await axios.get('/api', {
+    const response = await axios.get('', {
       params: {
         key: API_KEY,
         q: query,
